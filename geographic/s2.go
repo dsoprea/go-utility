0c66201f25d9d450765cecbf2e2da69c907eb5b2
Dustin Oprea <doprea@magicleap.com>
2019-02-24 22:38:41 -0500

    geographic/s2.go: Drop TestS2CellIdFromCoordinates; fairly redundant

--- geographic/s2.go
+++ geographic/s2.go
@@ -5,18 +5,7 @@ import (
 	"github.com/golang/geo/s2"
 )
 
-func S2CellIdFromCoordinates(latitude, longitude float64) uint64 {
-	ll := s2.LatLngFromDegrees(latitude, longitude)
-	cellId := s2.CellIDFromLatLng(ll)
-
-	if cellId.IsValid() == false {
-		log.Panicf("S2CellIdFromCoordinates: final cell-ID not valid: (%.6f,%.6f) -> (%d)", latitude, longitude, uint64(cellId))
-	}
-
-	return uint64(cellId)
-}
-
-func S2TokenFromCoordinates(latitude, longitude float64) string {
+func S2CellFromCoordinates(latitude, longitude float64) s2.CellID {
 	ll := s2.LatLngFromDegrees(latitude, longitude)
 	cellId := s2.CellIDFromLatLng(ll)
 
@@ -24,6 +13,5 @@ func S2TokenFromCoordinates(latitude, longitude float64) string {
 		log.Panicf("S2TokenFromCoordinates: final cell-ID not valid: (%.6f,%.6f) -> (%d)", latitude, longitude, uint64(cellId))
 	}
 
-	token := cellId.ToToken()
-	return token
+	return cellId
 }
--- geographic/s2_test.go
+++ geographic/s2_test.go
@@ -4,20 +4,11 @@ import (
 	"testing"
 )
 
-func TestS2CellIdFromCoordinates(t *testing.T) {
-	actualCellId := S2CellIdFromCoordinates(26.568629, -80.108965)
+func TestS2CellFromCoordinates(t *testing.T) {
+	actualCell := S2CellFromCoordinates(26.568629, -80.108965)
 	expectedCellId := uint64(9860956140720671831)
 
-	if actualCellId != expectedCellId {
-		t.Fatalf("Cell-ID not correct.")
-	}
-}
-
-func TestTokenFromCoordinates(t *testing.T) {
-	actualToken := S2TokenFromCoordinates(26.568629, -80.108965)
-	expectedToken := "88d9275d495ccc57"
-
-	if actualToken != expectedToken {
-		t.Fatalf("Token not correct: [%s] != [%s]", actualToken, expectedToken)
+	if uint64(actualCell) != expectedCellId {
+		t.Fatalf("Cell-ID not correct: (%d) != (%d)", uint64(actualCell), expectedCellId)
 	}
 }
