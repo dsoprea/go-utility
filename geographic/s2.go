d976f717574ee1ba943d71fcef0ad9df3dbec5c0
Dustin Oprea <doprea@magicleap.com>
2019-02-24 22:25:38 -0500

    geographic/s2.go: Add validity checks

--- geographic/s2.go
+++ geographic/s2.go
@@ -1,6 +1,7 @@
 package rigeo
 
 import (
+	"github.com/dsoprea/go-logging"
 	"github.com/golang/geo/s2"
 )
 
@@ -8,12 +9,20 @@ func S2CellIdFromCoordinates(latitude, longitude float64) uint64 {
 	ll := s2.LatLngFromDegrees(latitude, longitude)
 	cellId := s2.CellIDFromLatLng(ll)
 
+	if cellId.IsValid() == false {
+		log.Panicf("S2CellIdFromCoordinates: final cell-ID not valid: (%.6f,%.6f) -> (%d)", latitude, longitude, uint64(cellId))
+	}
+
 	return uint64(cellId)
 }
 
 func S2TokenFromCoordinates(latitude, longitude float64) string {
 	ll := s2.LatLngFromDegrees(latitude, longitude)
 	cellId := s2.CellIDFromLatLng(ll)
+
+	if cellId.IsValid() == false {
+		log.Panicf("S2TokenFromCoordinates: final cell-ID not valid: (%.6f,%.6f) -> (%d)", latitude, longitude, uint64(cellId))
+	}
 
 	token := cellId.ToToken()
 	return token
