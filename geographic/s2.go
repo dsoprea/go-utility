4223ad70def9f5ce750aa2092a8a946a6b63f365
Dustin Oprea <doprea@magicleap.com>
2019-02-24 22:13:43 -0500

    geographic/s2.go: Add TestTokenFromCoordinates

--- geographic/s2.go
+++ geographic/s2.go
@@ -1,12 +1,20 @@
 package rigeo
 
 import (
-    "github.com/golang/geo/s2"
+	"github.com/golang/geo/s2"
 )
 
 func S2CellIdFromCoordinates(latitude, longitude float64) uint64 {
-    ll := s2.LatLngFromDegrees(latitude, longitude)
-    cellId := s2.CellIDFromLatLng(ll)
+	ll := s2.LatLngFromDegrees(latitude, longitude)
+	cellId := s2.CellIDFromLatLng(ll)
 
-    return uint64(cellId)
+	return uint64(cellId)
+}
+
+func S2TokenFromCoordinates(latitude, longitude float64) string {
+	ll := s2.LatLngFromDegrees(latitude, longitude)
+	cellId := s2.CellIDFromLatLng(ll)
+
+	token := cellId.ToToken()
+	return token
 }
--- geographic/s2_test.go
+++ geographic/s2_test.go
@@ -1,14 +1,23 @@
 package rigeo
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestS2CellIdFromCoordinates(t *testing.T) {
-    actualCellId := S2CellIdFromCoordinates(26.568629, -80.108965)
-    expectedCellId := uint64(9860956140720671831)
+	actualCellId := S2CellIdFromCoordinates(26.568629, -80.108965)
+	expectedCellId := uint64(9860956140720671831)
 
-    if actualCellId != expectedCellId {
-        t.Fatalf("Cell-ID not correct.")
-    }
+	if actualCellId != expectedCellId {
+		t.Fatalf("Cell-ID not correct.")
+	}
+}
+
+func TestTokenFromCoordinates(t *testing.T) {
+	actualToken := S2TokenFromCoordinates(26.568629, -80.108965)
+	expectedToken := "88d9275d495ccc57"
+
+	if actualToken != expectedToken {
+		t.Fatalf("Token not correct: [%s] != [%s]", actualToken, expectedToken)
+	}
 }
