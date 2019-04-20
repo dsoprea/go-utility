a923c131a7410b838d8442520d9e9d3a7046d851
Dustin Oprea <myselfasunder@gmail.com>
2019-04-20 18:38:39 -0400

    reader_hash_proxy.go: Add Hash32 proxy

--- crypto/reader_hash_proxy.go
+++ crypto/reader_hash_proxy.go
@@ -44,3 +44,41 @@ func (rhp *ReaderHashProxy) Read(b []byte) (n int, err error) {
 func (rhp *ReaderHashProxy) Sum() []byte {
     return rhp.h.Sum(nil)
 }
+
+type ReaderHash32Proxy struct {
+    r io.Reader
+    h hash.Hash32
+}
+
+func NewReaderHash32Proxy(r io.Reader, h hash.Hash32) *ReaderHash32Proxy {
+    return &ReaderHash32Proxy{
+        r: r,
+        h: h,
+    }
+}
+
+func (rhp *ReaderHash32Proxy) Read(b []byte) (n int, err error) {
+    defer func() {
+        if state := recover(); state != nil {
+            err = log.Wrap(state.(error))
+        }
+    }()
+
+    n, err = rhp.r.Read(b)
+    if err != nil {
+        if err == io.EOF {
+            return 0, err
+        }
+
+        log.Panic(err)
+    }
+
+    n, err = rhp.h.Write(b[:n])
+    log.PanicIf(err)
+
+    return n, nil
+}
+
+func (rhp *ReaderHash32Proxy) Sum32() uint32 {
+    return rhp.h.Sum32()
+}
