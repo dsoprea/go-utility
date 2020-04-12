2021d59a1770a173a007c3b6ae465177f2932318
Dustin Oprea <dustin@randomingenuity.com>
2020-04-12 07:23:16 -0400

    Start ritesting package with redirect_tty

---
+++ testing/README.md
@@ -0,0 +1,9 @@
+[![GoDoc](https://godoc.org/github.com/dsoprea/go-utility/testing?status.svg)](https://godoc.org/github.com/dsoprea/go-utility/testing)
+[![Build Status](https://travis-ci.org/dsoprea/go-utility.svg?branch=master)](https://travis-ci.org/dsoprea/go-utility)
+[![Coverage Status](https://coveralls.io/repos/github/dsoprea/go-utility/badge.svg?branch=master)](https://coveralls.io/github/dsoprea/go-utility?branch=master)
+[![Go Report Card](https://goreportcard.com/badge/github.com/dsoprea/go-utility)](https://goreportcard.com/report/github.com/dsoprea/go-utility)
+
+# redirect_tty
+
+This will temporarily redirect the process TTY resources to support writing
+unit-tests directly against `main()` functions.
---
+++ testing/redirect_tty.go
@@ -0,0 +1,101 @@
+package ritesting
+
+import (
+	"os"
+
+	"github.com/dsoprea/go-logging"
+)
+
+type redirectedTty struct {
+	originalStdin  *os.File
+	newStdinWriter *os.File
+
+	originalStdout  *os.File
+	newStdoutReader *os.File
+
+	originalStderr  *os.File
+	newStderrReader *os.File
+}
+
+var (
+	rtty *redirectedTty
+)
+
+// RedirectTty redirects STDIN, STDOUT, and STDERR to alternative resources.
+// This allows us to create unit-tests for executables by directly calling
+// their main() entrypoints.
+func RedirectTty() {
+	if rtty != nil {
+		log.Panicf("TTY is already redirected")
+	}
+
+	rtty = &redirectedTty{
+		originalStdin:  os.Stdin,
+		originalStdout: os.Stdout,
+		originalStderr: os.Stderr,
+	}
+
+	var err error
+
+	// The caller can write to the new writer and it will be available on STDIN.
+
+	os.Stdin, rtty.newStdinWriter, err = os.Pipe()
+	log.PanicIf(err)
+
+	// Any output to STDOUT or STDERR will be available to the caller on the
+	// new readers.
+
+	rtty.newStdoutReader, os.Stdout, err = os.Pipe()
+	log.PanicIf(err)
+
+	rtty.newStderrReader, os.Stderr, err = os.Pipe()
+	log.PanicIf(err)
+}
+
+// RestoreTty restores original TTY resources.
+func RestoreTty() {
+	if rtty == nil {
+		log.Panicf("no TTY redirected; can not restore")
+	}
+
+	os.Stdin = rtty.originalStdin
+	os.Stdout = rtty.originalStdout
+	os.Stderr = rtty.originalStderr
+
+	rtty = nil
+}
+
+// StdinWriter returns a writer that can be used to feed STDIN data (if
+// redirected).
+func StdinWriter() *os.File {
+	if rtty == nil {
+		log.Panicf("TTY not redirected; STDIN writer not available")
+	}
+
+	return rtty.newStdinWriter
+}
+
+// StdoutReader returns a reader that can be used to read STDOUT output (if
+// redirected).
+func StdoutReader() *os.File {
+	if rtty == nil {
+		log.Panicf("TTY not redirected; STDOUT reader not available")
+	}
+
+	return rtty.newStdoutReader
+}
+
+// StderrReader returns a reader that can be used to read STDERR output (if
+// redirected).
+func StderrReader() *os.File {
+	if rtty == nil {
+		log.Panicf("TTY not redirected; STDERR reader not available")
+	}
+
+	return rtty.newStderrReader
+}
+
+// IsTtyRedirected returns whether the TTY is currently redirected.
+func IsTtyRedirected() bool {
+	return rtty != nil
+}
